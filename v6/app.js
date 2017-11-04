f87dad1c5295c33ab73cdb2f653993cb899526af
Sattar Hummatli <settarxan@gmail.com>
2017-11-04 12:11:27 +0000

    added login check middleware to my new coment routes

--- v6/app.js
+++ v6/app.js
@@ -85,7 +85,7 @@ app.get("/campgrounds/:id", function(req, res) {
 //=====================
 //COMMENTS ROUTES
 //=====================
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     //find campground by id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -96,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     })
 })
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -154,6 +154,20 @@ app.post(
     function(req, res) {
     
 })
+
+//logout route
+app.get("/logout", function(req, res) {
+    req.logout()
+    res.redirect("/campgrounds")
+})
+
+//middleware
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect("/login")
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server Has Started")
