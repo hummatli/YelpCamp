7e0daa2269bf2ecb28931d42d7a2e22f6263b60d
Sattar Hummatli <settarxan@gmail.com>
2017-11-04 11:38:20 +0000

    created register form, and register post route

--- v6/app.js
+++ v6/app.js
@@ -21,6 +21,17 @@ app.use(express.static(__dirname + "/public"))
 // Itit DB Configuration
 seedDB()
 
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty is cutest dog",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 //ROUTES
 app.get("/", function(req, res) {
@@ -102,6 +113,29 @@ app.post("/campgrounds/:id/comments", function(req, res) {
                 }
             })
         }
+    })
+})
+
+//============
+//AUTH ROUTES
+//===========
+
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register")
+})
+
+//will handle sign up logic 
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err)
+            return res.render("register")
+        } 
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds")
+        })
     })
 })
 
