da21bf9b01e5692c95b7492057dfaefddc87755c
Sattar Hummatli <settarxan@gmail.com>
2017-11-05 17:42:23 +0000

    Added edit and update route for comments

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -44,6 +44,31 @@ router.post("/", isLoggedIn, function(req, res) {
     })
 })
 
+//Edit Comment
+router.get("/:comment_id/edit", function(req, res) {
+        console.log("Inside edit")
+
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err) {
+            console.log(err)
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
+        }
+    })
+})
+
+//Update comment
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if(err) {
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
+
 //middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
