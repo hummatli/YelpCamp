2f06e2bf3d8efb83db3c615bd2f0e4ef8bb3ad39
Sattar Hummatli <settarxan@gmail.com>
2017-11-05 18:09:44 +0000

    Added authorization to comments, now edit and delete buttons visible only to user how has added them

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -45,12 +45,9 @@ router.post("/", isLoggedIn, function(req, res) {
 })
 
 //Edit Comment
-router.get("/:comment_id/edit", function(req, res) {
-        console.log("Inside edit")
-
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
-            console.log(err)
             res.redirect("back")
         } else {
             res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
@@ -59,7 +56,7 @@ router.get("/:comment_id/edit", function(req, res) {
 })
 
 //Update comment
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             res.redirect("back")
@@ -70,7 +67,7 @@ router.put("/:comment_id", function(req, res) {
 })
 
 //Destroy Comment
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
             res.redirect("back")
@@ -87,5 +84,24 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login")
 }
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect("back")
+            } else {
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next()
+                } else {
+                    res.redirect("back")
+                }
+            }
+        })
+    } else {
+        res.redirect("back")
+    }
+}
+
 
 module.exports = router
