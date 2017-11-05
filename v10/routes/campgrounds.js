8f5e4319598d969500d1c58ec2488c4358fba9be
Sattar Hummatli <settarxan@gmail.com>
2017-11-05 16:15:53 +0000

    Added delete routes and deleet button

--- v10/routes/campgrounds.js
+++ v10/routes/campgrounds.js
@@ -74,6 +74,17 @@ router.put("/:id", function(req, res) {
     })
 })
 
+//Destroy campground route
+router.delete("/:id", function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect("/campgrounds")
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
 //middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
